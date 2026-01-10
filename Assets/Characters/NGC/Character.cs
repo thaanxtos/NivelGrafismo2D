66ed2eb5ae09c3c61a933e7e6c749b4cf7a91ff1
thaanxtos <thaanxtos.mc@gmail.com>
2026-01-10 19:01:20 +0100

    acabé mi escena

---
+++ Assets/Characters/NGC/Character.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    int speed = 5;
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime);            
+        }
+        else if(Input.GetKey(KeyCode.D))
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime);
+        }
+    }
+}
